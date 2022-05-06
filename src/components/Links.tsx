25c2a1f7fb73bbf6506e6dcad2ba835ec7b5bcd0
Zachary Roland <zroland@talentplus.com>
2022-05-06 14:38:19 -0500

    adds link content

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,5 +1,40 @@
-const Links = () => {
-    return (<div>Links Go Here!</div>)
+import { Button, Grid } from "@mui/material";
+interface LinkObj {
+	text: string;
+	url: string;
 }
+
+type LinkProps = {
+	isXs: boolean;
+	isSm: boolean;
+}
+
+const Links = ({ isXs, isSm }: LinkProps) => {
+	const links: LinkObj[] = [
+		{
+			text: "Check out my resume",
+			url: "https://drive.google.com/file/d/1Yza--EjkHYr69XBa9KDQnmNzc_aSU7u7/view?usp=sharing",
+		},
+		{
+			text: "Listen to Bokr Tov",
+			url: "https://bokrtov.bandcamp.com/",
+		},
+	];
+	return (
+		<Grid
+			container
+			spacing={0}
+			direction="column"
+			alignItems={"center"}
+			justifyContent={"center"}
+		>
+			{links.map((link) => (
+				<Grid key={`${link.text} button`} item xs={12} sx={{ margin: "15px"}}>
+					<Button variant="contained" href={link.url} sx={{ width: "250px", height: "50px" }}>{link.text}</Button>
+				</Grid>
+			))}
+		</Grid>
+	);
+};
 
 export default Links;
