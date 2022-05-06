8357c34bf1f75387c7e9eba06a9ec0aa80639509
Zachary Roland <zroland@talentplus.com>
2022-05-06 14:37:59 -0500

    adds media queries, props, footer prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React from "react";
 import "@fontsource/roboto";
-import './App.css';
-import { Container, Typography } from "@mui/material";
+// import './App.css';
+import { Container, Grid, Typography } from "@mui/material";
 import Header from "./components/Header";
 import Links from "./components/Links";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { Box } from "@mui/system";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Typography>Some Text</Typography>
-    <Container>
-      <div>Some Text</div>
-      <Links />
-    </Container>
-    </>
-  );
+  const isXs: boolean = useMediaQuery('(max-width:400px)')
+	const isSm: boolean = useMediaQuery('(max-width:600px)');
+	return (
+		<Container sx={{ paddingTop: "48px", paddingBottom: "48px" }}>
+			<Header isXs={isXs} isSm={isSm}/>
+			<Links isXs={isXs} isSm={isSm}/>
+      {/* <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0}}>
+        <Grid container alignItems={"center"} justifyContent={"center"}>
+        <Grid item xs={12} 
+			spacing={0}
+			direction="column" alignItems={"center"} justifyContent={"center"}>
+        <Typography variant="caption">Hey, I made this! Isn't that cool?</Typography>
+        </Grid>
+        <Grid item xs={12}>
+        <Typography variant="caption"> If you want, you can check out the project on GitHub!</Typography>
+        </Grid>
+        </Grid>
+      </Box> */}
+		</Container>
+	);
 }
 
 export default App;
