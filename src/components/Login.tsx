de3d3d702f5612e860637842f5103a5dbce2f834
Zachary Roland <zroland@talentplus.com>
2022-05-13 18:09:29 -0500

    changes click to handleLogin fn

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,17 +5,17 @@ import { useNavigate } from "react-router-dom";
 type LoginProps = {
 	isXs: boolean;
 	isSm: boolean;
-    isAdmin: boolean;
-    setIsAdmin: Function;
+	isAdmin: boolean;
+	setIsAdmin: Function;
 };
 
 const Login = ({ isXs, isSm, isAdmin, setIsAdmin }: LoginProps) => {
 	const navigate = useNavigate();
-    const handleLogin = () => {
-        //TODO if email and username match log in, then do this
-        setIsAdmin(true);
-        navigate("/")
-    }
+	const handleLogin = () => {
+		//TODO if email and username match log in, then do this
+		setIsAdmin(true);
+		navigate("/");
+	};
 	return (
 		<Grid
 			container
@@ -49,7 +49,7 @@ const Login = ({ isXs, isSm, isAdmin, setIsAdmin }: LoginProps) => {
 						<TextField label="Password" color="info" sx={{ margin: "12px" }} />
 						<Grid item sx={{ marginTop: "24px" }}>
 							<Button
-								onClick={() => handleLogin()}
+								onClick={() => navigate("/")}
 								sx={{
 									margin: "12px",
 									borderColor: "secondary.main",
@@ -65,6 +65,7 @@ const Login = ({ isXs, isSm, isAdmin, setIsAdmin }: LoginProps) => {
 							<Button
 								className="MuiButton"
 								variant="contained"
+								onClick={() => handleLogin()}
 								sx={{ margin: "12px", border: `1px solid #6942ef` }}
 							>
 								Login
