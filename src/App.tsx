129a54237ead7705d558d4fc71c0d56207413b83
Zachary Roland <zroland@talentplus.com>
2022-05-13 11:42:09 -0500

    starts moving components into routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,38 @@ import Links from "./components/Links";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import darkTheme from "./common/styles";
 import Appbar from "./components/Appbar";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
 	const isXs: boolean = useMediaQuery("(max-width:420px)");
 	const isSm: boolean = useMediaQuery("(max-width:600px)");
 	return (
-    <ThemeProvider theme={darkTheme}>
-		<Container
-      maxWidth={false}
-			sx={{ paddingTop: "12px", paddingBottom: "48px", minWidth: "320px", height: "100vh" }}
-		>
-      <Appbar isXs={isXs} isSm={isSm} />
-			<Header isXs={isXs} isSm={isSm} />
-			<Links isXs={isXs} isSm={isSm} />
-			{/* <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0}}>
+		<ThemeProvider theme={darkTheme}>
+			<Container
+				maxWidth={false}
+				sx={{
+					paddingTop: "12px",
+					paddingBottom: "48px",
+					minWidth: "320px",
+					height: "100vh",
+				}}
+			>
+				<Appbar isXs={isXs} isSm={isSm} />
+				<Routes>
+					<Route
+						path="*"
+						element={
+							<>
+								<Header isXs={isXs} isSm={isSm} />
+								<Links isXs={isXs} isSm={isSm} />
+							</>
+						}
+					/>
+          <Route path="/login" element={<Login />}/>
+				</Routes>
+
+				{/* <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0}}>
         <Grid container alignItems={"center"} justifyContent={"center"}>
         <Grid item xs={12} 
 			spacing={0}
@@ -31,8 +49,8 @@ function App() {
         </Grid>
         </Grid>
       </Box> */}
-		</Container>
-    </ThemeProvider>
+			</Container>
+		</ThemeProvider>
 	);
 }
 
