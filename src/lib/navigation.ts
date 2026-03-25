25fb8efa748848246a623f725dd2c5c45036c6ab
Zachary Roland <zachary@Sionainns-MacBook-Pro.local>
2026-03-24 19:59:00 -0500

    feat: add ~/contact nav item

--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -3,4 +3,5 @@ export const navItems = [
   { to: "/about", label: "~/about" },
   { to: "/music", label: "~/music" },
   { to: "/projects", label: "~/projects" },
+  { to: "/contact", label: "~/contact" },
 ] as const;
